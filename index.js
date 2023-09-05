0714e1dd0f8ad6639b94684c5248b72a88f53f50
Dulce Ramirez <dinggy.dot@gmail.com>
2023-09-04 20:18:36 -0600

    verificar si la ruta es absoluta, verificar si el path existe, verificar extension y tests

--- index.js
+++ index.js
@@ -1,4 +1,30 @@
-module.exports = () => {
-  // ...
-  // cambios en la nueva rama
-};
+const path = require('path');
+const fs = require('fs').promises;
+
+const mdLinks = (inputPath, options) => {
+  let resolvedPath = inputPath;
+  if (!path.isAbsolute(inputPath)) { //if path is relative 
+    resolvedPath = path.resolve(inputPath); //takes inputPath and makes it absolute
+
+  }
+  return new Promise((resolve, reject) => {
+    //check if path exists
+    fs.access(resolvedPath)
+    .then(() => {
+      const fileExtension = path.extname(resolvedPath); //get file extension 
+      const markdownExtensions = ['.md', '.mkd', '.mdwn', '.mdown', '.mdtxt', '.mdtext', '.markdown', '.text'];
+      if (markdownExtensions.includes(fileExtension)){
+        resolve("The file is Markdown");
+      } else {
+        reject(new Error("Invalid file extension"))
+      }
+
+    })
+    .catch(err => {
+      reject(new Error("Path not found"))
+    });
+
+  })
+}
+
+module.exports = mdLinks;
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,10 +1,20 @@
-const mdLinks = require('../');
-
+const mdLinks = require('../index.js');
 
 describe('mdLinks', () => {
-
-  it('should...', () => {
-    console.log('FIX ME!');
+  /*it('should return a promise', () => {
+    expect(mdLinks()).toBe(typeof Promise);
+  });*/
+  it('should be a function', () => {
+    expect(typeof mdLinks).toBe('function')
+  });
+  it('should reject the promise if the path does not exist', () => {
+    const nonExistentPath = './this/path/dont/exist.md';
+    //Jest´s rejects method verifies that the promise is rejected 
+    return expect(mdLinks(nonExistentPath)).rejects.toThrow("Path not found");
+  });
+  it('should reject with an error if the file format is not valid', () => {
+    const fileFormatNotValid = './test/example.txt';
+    return expect(mdLinks(fileFormatNotValid)).rejects.toThrow("Invalid file extension");
   });
 
 });
