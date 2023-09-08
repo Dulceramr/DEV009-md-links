2a47a0c1d578c6b642c57865e77efd3e34d23ed3
Dulce Ramirez <dinggy.dot@gmail.com>
2023-09-07 23:04:00 -0600

    termino de hito1, falta mas tests, configuracion de eslint

--- index.js
+++ index.js
@@ -10,21 +10,38 @@ const mdLinks = (inputPath, options) => {
   return new Promise((resolve, reject) => {
     //check if path exists
     fs.access(resolvedPath)
-    .then(() => {
-      const fileExtension = path.extname(resolvedPath); //get file extension 
-      const markdownExtensions = ['.md', '.mkd', '.mdwn', '.mdown', '.mdtxt', '.mdtext', '.markdown', '.text'];
-      if (markdownExtensions.includes(fileExtension)){
-        resolve("The file is Markdown");
-      } else {
-        reject(new Error("Invalid file extension"))
-      }
-
-    })
-    .catch(err => {
-      reject(new Error("Path not found"))
-    });
-
-  })
+      .then(() => {
+        const fileExtension = path.extname(resolvedPath); //get file extension 
+        const markdownExtensions = ['.md', '.mkd', '.mdwn', '.mdown', '.mdtxt', '.mdtext', '.markdown', '.text'];
+        if (markdownExtensions.includes(fileExtension)) {
+          console.log("se leyo el archivo");
+          return fs.readFile(resolvedPath, 'utf8');
+        } else {
+          console.log("estamos en el else, no se leyo el arhcivo");
+          reject(new Error("Invalid file extension"));
+        }
+      })
+      .then(content => {
+        console.log("se llego a la segunda promesa");
+        const regex = /\[([^\]]+)\]\((http[s]?:\/\/[^\)]+)\)/g;
+        let match;
+        const links = [];
+        while (match = regex.exec(content)) {
+          links.push({
+            text: match[1],
+            href: match[2],
+            file: resolvedPath
+          });
+        }
+        console.log("se extrajeron los links")
+        console.log(links);
+        resolve(links); //resolve the promise with the array of links  
+      })
+      .catch(err => {
+        console.log("entramos en el catch");
+        reject(new Error("Path not found"));
+      });
+  });
 }
-
+mdLinks('README.md');
 module.exports = mdLinks;
